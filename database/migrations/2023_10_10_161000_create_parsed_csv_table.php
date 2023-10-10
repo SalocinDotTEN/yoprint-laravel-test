0b1a31138e07c55092e6f9732a88b6f204cd4850
Salocin.TEN <Salocin.TEN@gmail.com>
2023-10-11 00:18:48 +0800

    shift sqlite into directory and make new migration.

---
+++ database/migrations/2023_10_10_161000_create_parsed_csv_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parsed_csv', function (Blueprint $table) {
+            $table->id();
+            $table->integer('UNIQUE_KEY');
+            $table->string('PRODUCT_TITLE');
+            $table->string('PRODUCT_DESCRIPTION');
+            $table->string('STYLE#');
+            $table->string('SANMAR_MAINFRAME_COLOR');
+            $table->string('SIZE');
+            $table->string('COLOR_NAME');
+            $table->float('PIECE_PRICE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parsed_csv');
+    }
+};
