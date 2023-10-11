159578b720dcb3b40cb0fef8d3f76099e853592b
Salocin.TEN <Salocin.TEN@gmail.com>
2023-10-11 23:08:17 +0800

    install resumable js and hook it up. successful finally.

--- app/Http/Controllers/ProcessCSVController.php
+++ app/Http/Controllers/ProcessCSVController.php
@@ -2,21 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
+
+use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
+use Pion\Laravel\ChunkUpload\Exceptions\UploadMissingFileException;
+use Pion\Laravel\ChunkUpload\Exceptions\UploadFailedException;
 
 class ProcessCSVController extends Controller
 {
-    public function storeCsv(Request $request): RedirectResponse
+    public function storeCsv(Request $request)
     {
-        $request->validate([
-            'csv_file' => 'required|mimes:csv,txt',
+        // create the file receiver
+        $receiver = new FileReceiver("file", $request, HandlerFactory::classFromRequest($request));
+
+        // check if the upload is success, throw exception or return response you need
+        if ($receiver->isUploaded() === false) {
+            throw new UploadMissingFileException();
+        }
+
+        // receive the file
+        $save = $receiver->receive();
+
+        // check if the upload has finished (in chunk mode it will send smaller files)
+        if ($save->isFinished()) {
+            // save the file and return any response you need, current example uses `move` function. If you are
+            // not using move, you need to manually delete the file by unlink($save->getFile()->getPathname())
+            return $this->saveFile($save->getFile());
+        }
+
+        // we are in chunk mode, lets send the current progress
+        $handler = $save->handler();
+
+        return response()->json([
+            "done" => $handler->getPercentageDone(),
+            'status' => true
         ]);
+    }
 
-        $file = $request->file('csv_file');
-        $file->storeAs('csv', 'uploaded.csv');
+    protected function saveFile(UploadedFile $file)
+    {
+        // $fileName = $this->createFilename($file);
 
-        return redirect()->view('uploader');
+        // Group files by mime type
+        $mime = str_replace('/', '-', $file->getMimeType());
+
+        // Group files by the date (week
+        $dateFolder = date("Y-m-d");
+
+        // Build the file path
+        $filePath = "upload/{$mime}/{$dateFolder}";
+        $finalPath = storage_path("app/public/" . $filePath);
+
+        // move the file name
+        $file->move($finalPath, $file->getClientOriginalName());
+
+        return response()->json([
+            'path' => asset('storage/' . $filePath),
+            'name' => $file->getClientOriginalName(),
+            'mime_type' => $mime
+        ]);
     }
 }
--- resources/views/uploader.blade.php
+++ resources/views/uploader.blade.php
@@ -4,22 +4,124 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>YoPrint TEST</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/resumable.js/1.1.0/resumable.min.js" integrity="sha512-UWMGINgjUq/2sNur/d2LbiAX6IHmZkkCivoKSdoX+smfB+wB8f96/6Sp8ediwzXBXMXaAqymp6S55SALBk5tNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
 <body>
-    <div class="container mt-5">
-        <form action="/csv-uploader" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="csv_file">CSV File</label>
-                <input type="file" class="form-control" id="csv_file" name="csv_file">
+    <div class="container pt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h5>Upload CSV</h5>
+                    </div>
+                    <div class="card-body">
+                        <div id="upload-container" class="text-center">
+                            <button id="browseFile" class="btn btn-primary">Browse File</button>
+                        </div>
+                        <div style="display: none" class="progress mt-3" style="height: 25px">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%; height: 100%">75%</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary">Upload</button>
-        </form>
+        </div>
+        <div class="row justify-content-center mt-3">
+            <table class="table table-striped" id="fileInfo">
+                <thead>
+                    <tr>
+                        <th scope="col">Date</th>
+                        <th scope="col">File Name</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td id="fileDate"></td>
+                        <td id="fileName"></td>
+                        <td id="fileStatus"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script>
+        let browseFile = document.getElementById('browseFile');
+        let resumable = new Resumable({
+            target: "/csv-uploader",
+            query: {
+                _token: "{{ csrf_token() }}"
+            },
+            fileType: ['csv', 'txt'],
+            //chunkSize: 2 * 1024 * 1024, // default is 1*1024*1024, this should be less than your maximum limit in php.ini
+            headers: {
+                'Accept': 'application/json'
+            },
+            testChunks: false,
+            throttleProgressCallbacks: 1,
+        });
+
+        resumable.assignBrowse(browseFile);
+
+        resumable.on('fileAdded', function(file) { // trigger when file picked
+            showProgress();
+            resumable.upload() // to actually start uploading.
+        });
+
+        resumable.on('fileProgress', function(file) { // trigger when file progress update
+            updateProgress(Math.floor(file.progress() * 100));
+        });
+
+        resumable.on('fileSuccess', function(file, response) { // trigger when file upload complete
+            response = JSON.parse(response)
+
+            document.getElementById('fileDate').innerHTML = "";
+            document.getElementById('fileName').innerHTML = response.name;
+            document.getElementById('fileStatus').innerHTML = "Uploaded";
+
+            // if (response.mime_type.includes("image")) {
+            //     $('#imagePreview').attr('src', response.path + '/' + response.name).show();
+            // }
+
+            // if (response.mime_type.includes("video")) {
+            //     $('#videoPreview').attr('src', response.path + '/' + response.name).show();
+            // }
+
+            // $('.card-footer').show();
+        });
+
+        resumable.on('fileError', function(file, response) { // trigger when there is any error
+            alert('There is an error in uploading file. Please try again.')
+        });
+
+        let progress = document.getElementsByClassName('progress');
+
+        function showProgress() {
+            let progressBar = document.querySelector('.progress-bar');
+            progressBar.style.width = '0%';
+            progressBar.innerHTML = '0%';
+            progressBar.classList.remove('bg-success');
+            let progress = document.querySelector('.progress');
+            progress.style.display = 'block';
+        }
+
+        function updateProgress(value) {
+            let progressBar = document.querySelector('.progress-bar');
+            progressBar.style.width = `${value}%`;
+            progressBar.innerHTML = `${value}%`;
+
+            if (value === 100) {
+                document.querySelector('.progress-bar').classList.add('bg-success');
+            }
+        }
+
+        function hideProgress() {
+            progress.style.display = 'none';
+        }
+    </script>
 </body>
- <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+
 </html>
